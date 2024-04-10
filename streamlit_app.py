7922525d6c346cee632328b5ef7c96e29b792c93
Christine <156078562+christine2377@users.noreply.github.com>
2024-04-10 12:13:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ else :
 st.subheader("Densité des donateurs par départements")
 """**Définition de densité de donateurs**"""
 "Nombre de foyers imposés à l'impôt sur le revenu ayant déclaré un don en 2023 sur l'ensemble des foyers imposés"
-st.write(df5, width = 30, hide_index= True)
+df5
 ##
 buffer = BytesIO()
 with pd.ExcelWriter(buffer, engine = "xlsxwriter") as writer:
